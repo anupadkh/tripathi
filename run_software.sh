6aff73f41be9781db97a668fe6a2ce7b1d739848
Bishwash Tripathi <bishwashtripathi84848@gmail.com>
2023-07-12 15:03:47 +0545

    software

---
+++ run_software.sh
@@ -0,0 +1,5 @@
+source /Users/bishwashtripathi/software/venv_foam/bin/activate
+cd /Users/bishwashtripathi/software/tripathi
+git pull
+cp backup.sqlite3 db.sqlite3
+/Users/bishwashtripathi/software/tripathi/manage.py runserver 0.0.0.0:8000
